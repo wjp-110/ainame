06b700db8beabc9dc59e41a308104545f1b1b651
James Wang <59546039+wjp-110@users.noreply.github.com>
2026-03-31 14:45:52 +0800

    Update __init__.py

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,12 +1,12 @@
 from datetime import timedelta
 
 
-DB_URI = "mysql+aiomysql://root:Mysql%402026@127.0.0.1:3306/ainame?charset=utf8mb4"
+DB_URI = "mysql+aiomysql://root:123456@127.0.0.1:3306/ainame?charset=utf8mb4"
 
 # 邮箱相关配置
-MAIL_USERNAME="460019140@qq.com"
-MAIL_PASSWORD="pjlxmvwivulhbgeg"
-MAIL_FROM="460019140@qq.com"
+MAIL_USERNAME="xxxxxx@qq.com"
+MAIL_PASSWORD="xxxxxxxxxxxxxx"
+MAIL_FROM="xxxxxx@qq.com"
 MAIL_PORT=587
 MAIL_SERVER="smtp.qq.com"
 MAIL_FROM_NAME="测试ainame"
